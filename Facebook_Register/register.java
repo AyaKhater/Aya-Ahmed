35124b890a267314456cd82fdc6cd74e6d31f7dc
AhmedA5 <AhmedA5@E0442089.internal.vodafone.com>
2019-11-03 15:40:18 +0200

    Test Commit

--- Facebook_Register/register.java
+++ Facebook_Register/register.java
@@ -27,7 +27,10 @@ public class register {
 		day.click();
 		WebElement option1 = driver.findElement(By.xpath("//*[@id=\"day\"]/option[5]"));
 		option1.click();
-		//Test
+		//Test  
+		
+		
+		
 
 		WebElement month = driver.findElement(By.id("month")).click();
 		month.click();
